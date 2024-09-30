b03da28191e00ec959d10c00f7258b8cfa7a98a2
coraleaf0602 <zhangco@tcd.ie>
2024-09-30 16:49:16 +0800

    Add help command which shows the list of available commands

--- src/main/java/seedu/duke/Records.java
+++ src/main/java/seedu/duke/Records.java
@@ -9,6 +9,44 @@ public class Records {
         patients = new ArrayList<>();
     }
 
+    // Takes in an input string and determines whether to exit the program
+    public void exit(String input) {
+        if(input.equalsIgnoreCase("exit")) {
+            System.exit(0);
+        }
+    }
+
+    // Prints output for help command
+    public void help() {
+        System.out.println("+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Action    | Format                                | Example                         |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Help      | help                                  | help                            |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Add       | add n/NAME ic/NRIC [p/PHONE_NUMBER]   | add n/James Ho ic/S9534567A     |\n" +
+                "|           | [d/DIAGNOSIS] [m/MEDICATION]          | p/91234567 d/Asthma m/Albuterol |\n" +
+                "|           | [ha/HOME_ADDRESS] [dob/DATE_OF_BIRTH] | ha/NUS-PGPR dob/13121995        |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| List      | list                                  | list                            |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Find      | find NAME [KEYWORDS] OR               | find NRIC S1234                 |\n" +
+                "|           | find NRIC [KEYWORDS] OR               |                                 |\n" +
+                "|           | find PHONE_NUMBER [KEYWORDS] OR       |                                 |\n" +
+                "|           | find DIAGNOSIS [KEYWORDS] OR          |                                 |\n" +
+                "|           | find MEDICATION [KEYWORDS] OR         |                                 |\n" +
+                "|           | find HOME_ADDRESS [KEYWORDS] OR       |                                 |\n" +
+                "|           | find DATE_OF_BIRTH [KEYWORDS]         |                                 |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Delete    | delete NRIC                           | delete S9534567A                |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Save      | save                                  | save                            |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Retrieve/ | retrieve or import                    | retrieve                        |\n" +
+                "| Import    |                                       |                                 |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n" +
+                "| Exit      | exit                                  | exit                            |\n" +
+                "+-----------+---------------------------------------+---------------------------------+\n");
+    }
     public void delete(String nric) {
         int initialPatientSize = patients.size();
         if (patients.isEmpty()) {
