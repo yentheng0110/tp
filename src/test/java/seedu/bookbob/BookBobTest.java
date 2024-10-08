5cf6c0b46668513b9cb8294847bff92572d5a39a
coraleaf0602 <zhangco@tcd.ie>
2024-10-08 18:11:09 +0800

    Add BookBobTest file

---
+++ src/test/java/seedu/bookbob/BookBobTest.java
@@ -0,0 +1,12 @@
+package seedu.bookbob;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BookBobTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
