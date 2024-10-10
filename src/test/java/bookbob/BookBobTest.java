bd322d4f8aca55fb053624970b15ee40f789a9be
e0968962@u.nus.edu <e0968962@u.nus.edu>
2024-10-10 23:14:06 +0800

    Implement JUnit Test for [delete] command

--- src/test/java/bookbob/BookBobTest.java
+++ src/test/java/bookbob/BookBobTest.java
@@ -239,4 +239,13 @@ public class BookBobTest {
         assertEquals(expectedOutput,
                 outputStreamCaptor.toString().trim().replace(System.lineSeparator(), "\n"));
     }
+
+    //@@author yentheng0110
+    @Test
+    void testDelete_deleteNonExistingPatient_recordsSizeRemainsTheSame() throws IOException {
+        command.add("add n/Jack Wong ic/S9765432T p/87658976 d/Gastric m/Gaviscon ha/Bukit Gombak dob/06071997",
+                records);
+        command.delete("S9587690H", records);
+        assertEquals(1, records.getPatients().size());
+    }
 }
