90acb3118929eeb6670181538c6466949f88e4f9
g13nd0n-admin <e0957797@u.nus.edu>
2024-10-10 19:21:44 +0800

    Fix CI Issue for testFind and testDelete

--- src/test/java/bookbob/BookBobTest.java
+++ src/test/java/bookbob/BookBobTest.java
@@ -83,10 +83,13 @@ public class BookBobTest {
 
     @Test
     void testDelete() throws IOException{
-        command.add("add n/James-Ho ic/S9534567A p/91234567 d/Asthma m/Albuterol ha/NUS-PGPR dob/13121995", records);
+        command.add("add n/James-Ho ic/S9534567A p/91234567 d/Asthma m/Albuterol ha/NUS-PGPR dob/13121995"
+                , records);
         command.delete("S9534567A", records);
-        assertEquals("Patient James-Ho with NRIC S9534567A added.\n" +
-                "Patient James-Ho, S9534567A, has been deleted.", outputStreamCaptor.toString().trim());
+        String expectedOutput = "Patient James-Ho with NRIC S9534567A added.\n" +
+                "Patient James-Ho, S9534567A, has been deleted.";
+        assertEquals(expectedOutput, outputStreamCaptor.toString().trim().replace(System.lineSeparator(),
+                "\n"));
     }
 
     @Test
@@ -95,7 +98,8 @@ public class BookBobTest {
         command.list(records);
         assertEquals("Patient James-Ho with NRIC S9534567A added.\n" +
                 "Name: James-Ho, NRIC: S9534567A, Phone: 91234567, Diagnosis: Asthma, Medication: [Albuterol], " +
-                "Address: NUS-PGPR, DOB: 13121995", outputStreamCaptor.toString().trim());
+                "Address: NUS-PGPR, DOB: 13121995", outputStreamCaptor.toString().trim().replace(System.lineSeparator(),
+                "\n"));
     }
 
     @Test
