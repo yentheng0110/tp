3ab50fe2fe7631f8e289bb6ffd65a5e821addb57
coraleaf0602 <zhangco@tcd.ie>
2024-10-14 19:41:38 +0800

    Modify test function to match tp master branch

--- src/test/java/bookbob/BookBobTest.java
+++ src/test/java/bookbob/BookBobTest.java
@@ -393,13 +393,13 @@ public class BookBobTest {
         assertEquals(expectedOutput,
                   outputStreamCaptor.toString().trim().replace(System.lineSeparator(), "\n"));
     }
-    
+
     //@@author yentheng0110
     @Test
     void testAdd_addPatientWithoutNRIC_patientNotAdded() throws IOException {
         command.add("add n/Jane Tan", records);
         String expectedOutput =
-                "Please provide the NRIC for the patient named Jane Tan, then add the patient record again.";
+                "Please provide the patient's NRIC.";
         assertEquals(expectedOutput,
                 outputStreamCaptor.toString().trim().replace(System.lineSeparator(), "\n"));
     }
