7b7f995a0633e07d77e4847dd5915cafca2e9aa4
coraleaf0602 <zhangco@tcd.ie>
2024-10-21 18:09:32 +0800

    Add diagnosis and medication to Visit class

--- src/main/java/bookbob/entity/Visit.java
+++ src/main/java/bookbob/entity/Visit.java
@@ -2,12 +2,34 @@ package bookbob.entity;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Visit {
     private LocalDateTime visitDate;
+    private String diagnosis;
+    private List<String> medication;
 
-    public Visit(LocalDateTime visitDate) {
+    public Visit(LocalDateTime visitDate, String diagnosis, List<String> medication) {
         this.visitDate = visitDate;
+        this.diagnosis = "";
+        this.medication = new ArrayList<>();
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public List<String> getMedication() {
+        return medication;
+    }
+
+    public void setMedication(List<String> medication) {
+        this.medication = medication;
     }
 
     public LocalDateTime getVisitDate() {
@@ -21,6 +43,7 @@ public class Visit {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        return "Visited the clinic on: " + this.getVisitDate().format(formatter);
+        return "Visited the clinic on: " + this.getVisitDate().format(formatter) + ", Diagnosis: " + getDiagnosis() +
+                ", Medication: " + getMedication();
     }
 }
