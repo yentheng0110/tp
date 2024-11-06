4259ab5e57d42efaa81ccc63cdfaeaa287bb0c5f
e0968962@u.nus.edu <e0968962@u.nus.edu>
2024-11-06 21:46:58 +0800

    Update author tags

--- src/test/java/bookbob/entity/PatientTest.java
+++ src/test/java/bookbob/entity/PatientTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+//@author yentheng0110
 public class PatientTest {
     private Patient patient;
     private String name;
@@ -20,6 +21,7 @@ public class PatientTest {
     private ArrayList<String> medicalHistories;
     private ArrayList<Visit> visits;
 
+    //@author yentheng0110
     @BeforeEach
     public void setUp() {
         // Initialize test data
@@ -39,6 +41,7 @@ public class PatientTest {
         visits.add(new Visit(visitDate));
     }
 
+    //@author yentheng0110
     @Test
     public void testPatientConstructor_withAllPatientDetails_instantiatePatientObjectCorrectly() {
         // Create a Patient instance with the test data
@@ -56,6 +59,7 @@ public class PatientTest {
         assertEquals(visits, patient.getVisits());
     }
 
+    //@author yentheng0110
     @Test
     public void testPatientConstructor_withMandatoryFields_instantiatePatientObjectCorrectly() {
         patient = new Patient(name, nric, visits);
@@ -65,6 +69,7 @@ public class PatientTest {
         assertEquals(visits, patient.getVisits());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetName() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -73,6 +78,7 @@ public class PatientTest {
         assertEquals("Jane Doe", patient.getName());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetNric() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -81,6 +87,7 @@ public class PatientTest {
         assertEquals("S7654321B", patient.getNric());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetDateOfBirth() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -89,6 +96,7 @@ public class PatientTest {
         assertEquals("02021976", patient.getDateOfBirth());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetPhoneNumber() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -97,6 +105,7 @@ public class PatientTest {
         assertEquals("87654321", patient.getPhoneNumber());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetHomeAddress() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -105,6 +114,7 @@ public class PatientTest {
         assertEquals("Block 456 Bukit Batok West Avenue 8", patient.getHomeAddress());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetVisits() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -116,6 +126,7 @@ public class PatientTest {
         assertEquals(newVisits, patient.getVisits());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetAllergies() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -126,6 +137,7 @@ public class PatientTest {
         assertEquals(newAllergies, patient.getAllergies());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetSex() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -134,6 +146,7 @@ public class PatientTest {
         assertEquals("Female", patient.getSex());
     }
 
+    //@author yentheng0110
     @Test
     public void testSetMedicalHistories() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
@@ -144,6 +157,7 @@ public class PatientTest {
         assertEquals(newMedicalHistories, patient.getMedicalHistories());
     }
 
+    //@author yentheng0110
     @Test
     public void testToString() {
         patient = new Patient(name, nric, phoneNumber, dateOfBirth, homeAddress, allergies, sex, medicalHistories,
