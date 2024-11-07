e92f54c402954e46cfc9c109f168d702e3ad6ae8
g13nd0n-admin <e0957797@u.nus.edu>
2024-11-07 17:21:40 +0800

    Fix bugs in the commands

--- src/main/java/bookbob/Main.java
+++ src/main/java/bookbob/Main.java
@@ -48,7 +48,7 @@ public class Main {
 
             case "exit":
                 logger.log(Level.INFO, "Processing exit command");
-                try{
+                try {
                     commandHandler.removePastAppointments(appointmentRecord);
                     commandHandler.exit(input);
                     logger.log(Level.INFO, "Successfully processed exit command");
@@ -136,6 +136,9 @@ public class Main {
                         System.out.println("Please specify an NRIC to delete.");
                         logger.log(Level.INFO, "Empty NRIC inputted");
                     }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    logger.log(Level.WARNING, "Error processing deletion");
+                    System.out.println("Error in deleting patient record, please provide the nric");
                 } catch (Exception e) {
                     logger.log(Level.WARNING, "Error processing deletion");
                     System.out.println("Error in deleting files, specific error: " + e.getMessage());
@@ -144,7 +147,7 @@ public class Main {
 
             case "help":
                 logger.log(Level.INFO, "Processing help command");
-                try{
+                try {
                     commandHandler.help();
                     logger.log(Level.INFO, "Successfully processed help command");
                 } catch (Exception e) {
@@ -199,7 +202,7 @@ public class Main {
 
             case "listAppointments":
                 logger.log(Level.INFO, "Processing list appointments command");
-                try{
+                try {
                     commandHandler.listAppointments(appointmentRecord);
                     logger.log(Level.INFO, "Successfully processed list appointments command");
                 } catch (DateTimeParseException e) {
@@ -228,6 +231,9 @@ public class Main {
                 } catch (IllegalArgumentException e) {
                     logger.log(Level.WARNING, "Error processing deletion of appointment command");
                     System.out.println("Error in deleting appointment, missing nric, date or time.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    logger.log(Level.WARNING, "Error processing deletion of appointment command");
+                    System.out.println("Error in deleting appointment, missing nric, date or time.");
                 } catch (Exception e) {
                     logger.log(Level.WARNING, "Error processing deletion of appointment");
                     System.out.println("Error in deleting appointment, specific error: " + e.getMessage());
@@ -245,6 +251,9 @@ public class Main {
                 } catch (DateTimeException e) {
                     logger.log(Level.WARNING, "Error processing appointment command", e);
                     System.out.println("Error in adding appointment, wrong time format");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    logger.log(Level.WARNING, "Error processing appointment command", e);
+                    System.out.println("Error in adding appointment, missing nric, date and time.");
                 } catch (Exception e) {
                     logger.log(Level.SEVERE, "Error processing find command", e);
                     System.out.println("An error occurred while processing the find command: " + e.getMessage());
