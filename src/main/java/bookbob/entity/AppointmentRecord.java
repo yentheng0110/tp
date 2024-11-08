c9357cbf1ba78cd8e417a5e2900fd057f7e6cbb3
coraleaf0602 <zhangco@tcd.ie>
2024-11-09 04:00:11 +0900

    Modify AppointmentRecord class to include FileOperation interface

--- src/main/java/bookbob/entity/AppointmentRecord.java
+++ src/main/java/bookbob/entity/AppointmentRecord.java
@@ -1,15 +1,20 @@
 package bookbob.entity;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 //@@author G13nd0n
-public class AppointmentRecord {
+public class AppointmentRecord implements FileOperation {
     private List<Appointment> appointments;
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
@@ -133,6 +138,60 @@ public class AppointmentRecord {
             }
         }
         return nextAvailableTime;
+    }
+
+    @Override
+    public void initFile(String appointmentFilePath) {
+        try {
+            String directoryName = "data";
+            String currentDirectory = System.getProperty("user.dir");
+            String directory = currentDirectory + File.separator + directoryName;
+            File directoryFile = new File(directory);
+
+            if(directoryFile.mkdirs()) {           //directory was not created
+                File file = new File(appointmentFilePath);
+                file.createNewFile();              //create new data file
+            } else {                               //directory already created
+                File file = new File(appointmentFilePath);
+                if (file.createNewFile()) {         //file was not created
+                } else {
+                    this.retrieveData(appointmentFilePath);
+                }
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void autosave(String appointmentFilePath) throws IOException, IOException {
+        List<Appointment> appointments = this.getAppointments();
+        FileWriter fw = new FileWriter(appointmentFilePath);
+        for (Appointment appointment : appointments) {
+            String toWrite = appointment.convertPatientToOutputText();
+            fw.write(toWrite + "\n");
+        }
+        fw.close();
+    }
+
+    @Override
+    public void retrieveData(String appointmentFilePath) {
+        try {
+            File file = new File(appointmentFilePath);
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] data = line.split("\\|");
+                String name = data[0].substring(6).trim();
+                String nric = data[1].substring(6).trim();
+                String date = data[2].substring(6).trim();
+                String time = data[3].substring(6).trim();
+                Appointment appointment = new Appointment(name, nric, date, time);
+                this.addAppointment(appointment);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
