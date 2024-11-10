5b23114023592b7ef20964c814409a6b1cb668f4
kaboomzxc <ong.junzheng@u.nus.edu>
2024-11-11 05:59:17 +0800

    fix CI

--- src/main/java/bookbob/parser/Parser.java
+++ src/main/java/bookbob/parser/Parser.java
@@ -28,29 +28,29 @@ public class Parser {
 
         String command = inputArray[0];
 
-    try{
-        switch (command) {
-        case "help":
-            if (inputArray.length > 1) {
-                System.out.println("Unknown command. Type 'help' for a list of commands.");
-                logger.log(Level.INFO, "Unknown command received: {0}", command);
+        try{
+            switch (command) {
+            case "help":
+                if (inputArray.length > 1) {
+                    System.out.println("Unknown command. Type 'help' for a list of commands.");
+                    logger.log(Level.INFO, "Unknown command received: {0}", command);
+                    break;
+                }
+                logAndExecute("help", commandHandler::help);
                 break;
-            }
-            logAndExecute("help", commandHandler::help);
-            break;
 
-        case "add":
-            logAndExecute("add", () -> {
+                case "add":
+                logAndExecute("add", () -> {
                 try {
                     commandHandler.add(input, records);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
-            break;
+                break;
 
-        case "delete":
-            logAndExecute("delete", () -> {
+            case "delete":
+                logAndExecute("delete", () -> {
                 if (inputArray.length > 1) {
                     String nric = inputArray[1].trim();
                     try {
@@ -63,34 +63,34 @@ public class Parser {
                     logger.log(Level.INFO, "Empty NRIC inputted for delete command");
                 }
             });
-            break;
-
-        case "list":
-            if (inputArray.length > 1) {
-                System.out.println("Unknown command. Type 'help' for a list of commands.");
-                logger.log(Level.INFO, "Unknown command received: {0}", command);
                 break;
-            }
-            logAndExecute("list", () -> commandHandler.list(records));
-            break;
 
-        case "edit":
-            logAndExecute("edit", () -> {
+            case "list":
+                if (inputArray.length > 1) {
+                    System.out.println("Unknown command. Type 'help' for a list of commands.");
+                    logger.log(Level.INFO, "Unknown command received: {0}", command);
+                    break;
+                }
+                logAndExecute("list", () -> commandHandler.list(records));
+                break;
+
+            case "edit":
+                logAndExecute("edit", () -> {
                 try {
                     commandHandler.edit(input, records);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
-            break;
+                break;
 
-        case "find":
-            logAndExecute("find", () -> commandHandler.find(input, records));
-            break;
+            case "find":
+                logAndExecute("find", () -> commandHandler.find(input, records));
+                break;
 
-        //@@author kaboomzxc
-        case "addVisit":
-            logAndExecute("addVisit", () -> {
+            //@@author kaboomzxc
+            case "addVisit":
+                logAndExecute("addVisit", () -> {
                 try {
                     commandHandler.addVisit(input, records);
                 } catch (IOException e) {
@@ -99,91 +99,91 @@ public class Parser {
                     throw new RuntimeException("Failed to save data", e);
                 }
             });
-            break;
+                break;
 
-        case "editVisit":
-            logAndExecute("editVisit", () -> {
+            case "editVisit":
+                logAndExecute("editVisit", () -> {
                 try {
                     commandHandler.editVisit(input, records);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
-            break;
+                break;
 
-        case "appointment":
-            logAndExecute("appointment", () -> {
+            case "appointment":
+                logAndExecute("appointment", () -> {
                 try {
                     commandHandler.appointment(input, appointmentRecord);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
-            break;
-
-        case "listAppointments":
-            if (inputArray.length > 1) {
-                System.out.println("Unknown command. Type 'help' for a list of commands.");
-                logger.log(Level.INFO, "Unknown command received: {0}", command);
                 break;
-            }
-            logAndExecute("listAppointments", () -> commandHandler.listAppointments(appointmentRecord));
-            break;
 
-        case "deleteAppointment":
-            logAndExecute("deleteAppointment", () -> {
+            case "listAppointments":
+                if (inputArray.length > 1) {
+                    System.out.println("Unknown command. Type 'help' for a list of commands.");
+                    logger.log(Level.INFO, "Unknown command received: {0}", command);
+                    break;
+                }
+                logAndExecute("listAppointments", () -> commandHandler.listAppointments(appointmentRecord));
+                break;
+
+            case "deleteAppointment":
+                logAndExecute("deleteAppointment", () -> {
                 try {
                     commandHandler.deleteAppointment(input, appointmentRecord);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
-            break;
-
-        case "findAppointment":
-            if (inputArray.length > 1) {
-                logAndExecute("findAppointment", () -> commandHandler.
-                        findAppointment(inputArray[1], appointmentRecord));
-            } else {
-                System.out.println("Please provide search criteria for findAppointment.");
-                logger.log(Level.INFO, "Missing criteria for findAppointment");
-            }
-            break;
-
-        case "findVisit":
-            if (inputArray.length > 1) {
-                logAndExecute("findVisit", () -> commandHandler.findVisitByIc(inputArray[1], records));
-            } else {
-                System.out.println("Please provide NRIC for findVisit.");
-                logger.log(Level.INFO, "Missing NRIC for findVisit");
-            }
-            break;
-
-        case "findMedication":
-            if (inputArray.length > 1) {
-                logAndExecute("findMedication", () -> commandHandler.findVisitByMedication(inputArray[1], records));
-            } else {
-                System.out.println("Please provide medication for findMedication.");
-                logger.log(Level.INFO, "Missing medication for findMedication");
-            }
-            break;
-
-        case "findDiagnosis":
-            if (inputArray.length > 1) {
-                logAndExecute("findDiagnosis", () -> commandHandler.findVisitByDiagnosis(inputArray[1], records));
-            } else {
-                System.out.println("Please provide diagnosis for findDiagnosis.");
-                logger.log(Level.INFO, "Missing diagnosis for findDiagnosis");
-            }
-            break;
-
-        case "exit":
-            if (inputArray.length > 1) {
-                System.out.println("Unknown command. Type 'help' for a list of commands.");
-                logger.log(Level.INFO, "Unknown command received: {0}", command);
                 break;
-            }
-            logAndExecute("exit", () -> {
+
+            case "findAppointment":
+                if (inputArray.length > 1) {
+                    logAndExecute("findAppointment", () -> commandHandler.
+                        findAppointment(inputArray[1], appointmentRecord));
+                } else {
+                    System.out.println("Please provide search criteria for findAppointment.");
+                    logger.log(Level.INFO, "Missing criteria for findAppointment");
+                }
+                break;
+
+            case "findVisit":
+                if (inputArray.length > 1) {
+                    logAndExecute("findVisit", () -> commandHandler.findVisitByIc(inputArray[1], records));
+                } else {
+                    System.out.println("Please provide NRIC for findVisit.");
+                    logger.log(Level.INFO, "Missing NRIC for findVisit");
+                }
+                break;
+
+            case "findMedication":
+                if (inputArray.length > 1) {
+                    logAndExecute("findMedication", () -> commandHandler.findVisitByMedication(inputArray[1], records));
+                } else {
+                    System.out.println("Please provide medication for findMedication.");
+                    logger.log(Level.INFO, "Missing medication for findMedication");
+                }
+                break;
+
+            case "findDiagnosis":
+                if (inputArray.length > 1) {
+                    logAndExecute("findDiagnosis", () -> commandHandler.findVisitByDiagnosis(inputArray[1], records));
+                } else {
+                    System.out.println("Please provide diagnosis for findDiagnosis.");
+                    logger.log(Level.INFO, "Missing diagnosis for findDiagnosis");
+                }
+                break;
+
+            case "exit":
+                if (inputArray.length > 1) {
+                    System.out.println("Unknown command. Type 'help' for a list of commands.");
+                    logger.log(Level.INFO, "Unknown command received: {0}", command);
+                    break;
+                }
+                logAndExecute("exit", () -> {
                 try {
                     commandHandler.removePastAppointments(appointmentRecord);
                 } catch (IOException e) {
@@ -191,19 +191,19 @@ public class Parser {
                 }
                 commandHandler.exit(input);
             });
-            break;
+                break;
 
-        default:
-            System.out.println("Unknown command. Type 'help' for a list of commands.");
-            logger.log(Level.INFO, "Unknown command received: {0}", command);
-            break;
+            default:
+                System.out.println("Unknown command. Type 'help' for a list of commands.");
+                logger.log(Level.INFO, "Unknown command received: {0}", command);
+                break;
+            }
+            return true;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error in command execution", e);
+            System.out.println("An error occurred. Please enter another command.");
+            return true;
         }
-        return true;
-    } catch (Exception e) {
-        logger.log(Level.SEVERE, "Error in command execution", e);
-        System.out.println("An error occurred. Please enter another command.");
-        return true;
-    }
     }
 
     private static void logAndExecute(String commandName, Runnable action) {
